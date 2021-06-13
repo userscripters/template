0ebf0b5c2663a5cff24784a7c68f6e8aba9d03fc
Oleg Valter <o.a.philippov@gmail.com>
2021-06-13 22:30:20 +0300

    TEMP commit

--- dist/headers.js
+++ dist/headers.js
@@ -1,11 +1,10 @@
-
 // ==UserScript==
-// @author Jane Doe
-// @description template repository for userscripters' projects
-// @homepage https://github.com/userscripters/template#readme
-// @name template
-// @source git+https://github.com/userscripters/template.git
-// @supportURL https://github.com/userscripters/template/issues
-// @version 1.0.0
-// @contributors John Doe
+// @author           Jane Doe
+// @contributors     John Doe
+// @description      template repository for userscripters' projects
+// @homepage         https://github.com/userscripters/template#readme
+// @name             template
+// @source           git+https://github.com/userscripters/template.git
+// @supportURL       https://github.com/userscripters/template/issues
+// @version          1.0.0
 // ==/UserScript==
