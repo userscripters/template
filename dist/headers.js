8be4b0403acac7697d145319fa7ff6155c550e06
Oleg Valter <o.a.philippov@gmail.com>
2021-07-21 21:12:53 +0300

    rebuilt sample dist

--- dist/headers.js
+++ dist/headers.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @author           Jane Doe
-// @contributors     John Doe
+// @author           Jane Doe <jane@doe.com>
+// @contributors     John Doe <john@doe.com>
 // @description      template repository for userscripters' projects
 // @homepage         https://github.com/userscripters/template#readme
 // @name             template
+// @namespace        userscripters
 // @source           git+https://github.com/userscripters/template.git
 // @supportURL       https://github.com/userscripters/template/issues
 // @version          1.0.0
--- dist/legacy/index.js
+++ dist/legacy/index.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @author           Jane Doe <jane@doe.com>
+// @contributors     John Doe <john@doe.com>
+// @description      template repository for userscripters' projects
+// @homepage         https://github.com/userscripters/template#readme
+// @name             template
+// @namespace        userscripters
+// @source           git+https://github.com/userscripters/template.git
+// @supportURL       https://github.com/userscripters/template/issues
+// @version          1.0.0
+// ==/UserScript==
+
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
